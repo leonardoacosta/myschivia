bee889fc3dd3e710f4ae4cc9ba53b162be278255
Leonardo Acosta <leo@leonardoacosta.dev>
2024-09-26 00:15:54 -0500

    Chat has spoken

--- apps/admin/src/app/(auth)/events/page.tsx
+++ apps/admin/src/app/(auth)/events/page.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
 import {
   CircleIcon,
+  Pin,
   PlusCircle,
   PlusIcon,
   StarIcon,
@@ -20,103 +24,175 @@ import {
   CardHeader,
   CardTitle,
 } from "@tribal-cities/ui/card";
-import { Tabs, TabsContent } from "@tribal-cities/ui/tabs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@tribal-cities/ui/dropdown-menu";
+import { Label } from "@tribal-cities/ui/label";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@tribal-cities/ui/tabs";
 
 import { api } from "~/trpc/react";
 
 export default function Page() {
   const router = useRouter();
   const { data: auth } = api.auth.getSession.useQuery();
-  const [events] = api.event.all.useSuspenseQuery();
+  const [date, setDate] = useState<Date | null>(null);
+  const [events] = api.event.all.useSuspenseQuery({ day: date });
+  const [dates] = api.event.allDates.useSuspenseQuery();
 
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-      <Tabs defaultValue="all">
-        <div className="flex items-center">
-          <div className="ml-auto flex items-center gap-2">
-            <Link href="/events/create">
-              <Button size="sm" className="h-7 gap-1">
-                <PlusCircle className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                  Register Event
-                </span>
-              </Button>
-            </Link>
-          </div>
+      <div className="flex items-center">
+        <div className="ml-auto flex items-center gap-2">
+          <Link href="/events/create">
+            <Button size="sm" className="h-7 gap-1">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Register Event
+              </span>
+            </Button>
+          </Link>
         </div>
-        <TabsContent value="all">
-          <Card x-chunk="dashboard-06-chunk-0">
-            <div className="flex justify-between">
-              <CardHeader>
-                <CardTitle>📅 Events</CardTitle>
-                <CardDescription>
-                  All events in the system, so far 👀
-                </CardDescription>
-              </CardHeader>
-            </div>
-            <CardContent>
-              <div className="grid gap-2">
-                {events.map((event) => (
-                  <Card
-                    onClick={() => {
-                      if (auth?.user.id === event.user.id)
-                        router.push(`/events/edit/${event.id}`);
-                      else router.push(`/events/view/${event.id}`);
-                    }}
-                    className="hover:cursor-pointer hover:bg-muted/50"
-                  >
-                    <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
-                      <div className="space-y-1">
-                        <CardTitle>{event.name}</CardTitle>
-                        <CardDescription>{event.description}</CardDescription>
-                      </div>
-                      {/* <Button variant="secondary" className="px-3 shadow-none">
-                        <StarIcon className="mr-2 h-4 w-4" />
-                        Save
-                      </Button> */}
-                    </CardHeader>
-                    <CardContent>
-                      <div className="flex space-x-4 text-sm text-muted-foreground">
-                        <div className="flex items-center">
-                          <CircleIcon className="mr-1 h-3 w-3 fill-sky-400 text-sky-400" />
-                          {event.type}
-                        </div>
-                        <div className="flex items-center">
-                          <User className="mr-1 h-3 w-3" />
-                          {event.user.alias}
-                        </div>
-                        <div className="flex items-center">
-                          <Tent className="mr-1 h-3 w-3" />
-                          {event.campName || "Self"}
-                        </div>
-                      </div>
-                      <div className="mt-2 flex space-x-4 text-sm text-muted-foreground">
-                        <div className="flex items-center gap-2">
-                          <span>
-                            From:
-                            <span>
-                              {" "}
-                              {format(event.startDate, "EEE")} @{" "}
-                              {event.startTime}
-                            </span>
-                          </span>
-                          <span>
-                            To:
-                            <span>
-                              {" "}
-                              {format(event.endDate, "EEE")} @ {event.endTime}
-                            </span>
-                          </span>
-                        </div>
-                      </div>
-                    </CardContent>
-                  </Card>
-                ))}
-              </div>
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
+      </div>
+      <Card x-chunk="dashboard-06-chunk-0">
+        <div className="flex justify-between">
+          <CardHeader>
+            <CardTitle>📅 Events</CardTitle>
+            <CardDescription>
+              All events in the system, so far 👀
+            </CardDescription>
+          </CardHeader>
+          <CardHeader>
+            <CardTitle className="flex items-center justify-center gap-2">
+              <Label>Filter by date</Label>
+              <CardDescription>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="secondary" className="px-3 shadow-none">
+                      {date ? format(date, "E LLL dd") : "All"}
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem
+                      onClick={() => setDate(null)}
+                      className="hover:bg-muted/50"
+                    >
+                      All
+                    </DropdownMenuItem>
+                    {dates.map((d) => (
+                      <DropdownMenuItem
+                        onClick={() => setDate(new Date(d))}
+                        className="hover:bg-muted/50"
+                      >
+                        {format(d, "E LLL dd")}
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </CardDescription>
+            </CardTitle>
+          </CardHeader>
+        </div>
+        <CardContent>
+          <Tabs defaultValue="all">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="all">All</TabsTrigger>
+              <TabsTrigger value="schedule">My Schedule</TabsTrigger>
+            </TabsList>
+            <TabsContent value="schedule">
+              <CardHeader>Coming soon</CardHeader>
+            </TabsContent>
+            <TabsContent value="all">
+              {events.map((evs) => {
+                const eventsOfDay = evs[1]!;
+                return (
+                  <div className="mb-4">
+                    <div>
+                      {format((eventsOfDay[0] as any)?.startDate, "E LLL dd")}
+                    </div>
+
+                    <div className="grid gap-2">
+                      {(eventsOfDay as any)?.map((ev: any) => (
+                        <Card
+                          onClick={() => {
+                            if (auth?.user.id === ev.createdById)
+                              router.push(`/events/edit/${ev.id}`);
+                            else router.push(`/events/view/${ev.id}`);
+                          }}
+                          className="hover:cursor-pointer hover:bg-muted/50"
+                        >
+                          <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+                            <div className="space-y-1">
+                              <CardTitle>{ev.name}</CardTitle>
+                              <CardDescription>
+                                {ev.description}
+                              </CardDescription>
+                            </div>
+                            <Button
+                              variant="secondary"
+                              className="px-3 shadow-none"
+                            >
+                              <StarIcon className="mr-2 h-4 w-4" />
+                              Save
+                            </Button>
+                          </CardHeader>
+                          <CardContent>
+                            {/* <div className="text-xs font-medium">{item.subject}</div> */}
+                            <div className="flex space-x-4 text-sm text-muted-foreground">
+                              <div className="flex items-center">
+                                <CircleIcon className="mr-1 h-3 w-3 fill-sky-400 text-sky-400" />
+                                {ev.type}
+                              </div>
+                              <div className="flex items-center">
+                                <User className="mr-1 h-3 w-3" />
+                                {ev.user.alias}
+                              </div>
+                              <div className="flex items-center">
+                                <Tent className="mr-1 h-3 w-3" />
+                                {ev.campName || "Self"}
+                              </div>
+                              <div className="flex items-center">
+                                <Pin className="mr-1 h-3 w-3" />
+                                {ev.location}
+                              </div>
+                            </div>
+                            <div className="mt-2 flex space-x-4 text-sm text-muted-foreground">
+                              <div className="flex items-center gap-2">
+                                <span>
+                                  From:
+                                  <span>
+                                    {" "}
+                                    {format(ev.startDate, "EEE")} @{" "}
+                                    {ev.startTime}
+                                  </span>
+                                </span>
+                                <span>
+                                  To:
+                                  <span>
+                                    {" "}
+                                    {format(ev.endDate, "EEE")} @ {ev.endTime}
+                                  </span>
+                                </span>
+                              </div>
+                            </div>
+                          </CardContent>
+                        </Card>
+                      ))}
+                    </div>
+                  </div>
+                );
+              })}
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+      </Card>
     </main>
   );
 }
--- packages/api/src/router/event.ts
+++ packages/api/src/router/event.ts
@@ -6,20 +6,52 @@ import {
   CreateEventSchema,
   Event,
   UpdateEventSchema,
+  User,
 } from "@tribal-cities/db/schema";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
 
 export const eventRouter = {
-  all: publicProcedure.query(({ ctx }) =>
-    ctx.db.query.Event.findMany({
-      orderBy: asc(Event.startDate),
-      with: {
-        user: true,
-        // camp: true,
-      },
+  all: publicProcedure
+    .input(
+      z.object({
+        day: z.date().nullable(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const events = await ctx.db.query.Event.findMany({
+        where: input.day ? eq(Event.startDate, input.day) : undefined,
+        orderBy: [asc(Event.startTime), asc(Event.startTime)], // asc(Event.startTime),
+        with: {
+          user: true,
+          // camp: true,
+        },
+      });
+      // group by date
+      const eventsByDayObject = events.reduce(
+        (acc, event) => {
+          const key = event.startDate.toISOString().split("T")[0]!;
+          acc[key] = acc[key] || [];
+          acc[key].push(event);
+          return acc;
+        },
+        {} as Record<string, typeof events>,
+      );
+      let eventsByDay = Object.entries(eventsByDayObject).map(
+        ([day, events]) => [day, events],
+      );
+
+      // sort by date
+      eventsByDay = eventsByDay.sort(([a], [b]) => (a! < b! ? -1 : 1));
+      return eventsByDay;
     }),
-  ),
+  allDates: publicProcedure.query(async ({ ctx }) => {
+    const events = await ctx.db.query.Event.findMany();
+    const dates = events
+      .sort((a, b) => a.startDate.getDate() - b.startDate.getDate())
+      .map((event) => event.startDate.toString());
+    return Array.from(new Set(dates));
+  }),
 
   byId: publicProcedure
     .input(z.object({ id: z.string() }))
